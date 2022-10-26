5434358837cf9e09ce7dfe0c51d796eb05b57b70
waseem <waseem_6957@yahoo.com>
2022-10-27 02:57:56 +0530

    Password Change Module. Set Monthly Budget. Record and Expense. Date Wise Budgetary Logs.

--- src/Operations.java
+++ src/Operations.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Operations {
 
@@ -12,62 +13,6 @@ public class Operations {
     private String file_credentials = "src/credentials.txt";
     private String file_monthly_budget = "src/budget.txt";
     private String file_expenses = "src/expenses.txt";
-
-/*    public void addBudget() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("ENTER YOUR BUDGET AMOUNT:");
-        float amount = sc.nextFloat();
-        System.out.println("ENTER DATE(DD/MM/YYYY):");
-        String dateAsString = sc.nextLine();
-        Date date = DateUtil.getDate(dateAsString);
-
-        //Add Expense details in expense obj
-
-        Expense exp = new Expense();
-        exp.setAmount(amount);
-        exp.setDate(date);
-        repo.expList.add(Exp);
-        System.out.println("Success:Expense Added");
-    }
-
-    private void onExpenseList() {
-        System.out.println("Expense Listing...");
-        List<Expense> expList = repo.expList;
-        for (int i = 0; i < expList.size(); i++) {
-
-        }
-		
-		*//*double expense;
-		
-		 double totalExpenses = 0.0;
-		do {
-			System.out.print("Enter an expense, or 0 to calculate balance:");
-	        expense = sc.nextDouble();
-
-	        totalExpenses += expense;
-
-	    } while (expense >0);
-
-	    Display the amount after expenses.
-	    double balance = calculateAmountOverBudget(monthlyBudget, totalExpenses);
-	    if (balance < 0) {
-	        System.out.println("You are OVER budget by Rupees"
-	                + Math.abs(balance));
-	    } else if (balance > 0) {
-	        System.out.println("You are UNDER budget by Rupees"
-	                + (balance));
-	    } else {
-	        System.out.println("You spent the amount of:"+balance);
-	    }
-	}
-
-	static double calculateAmountOverBudget(double monthlyBudget,
-	        double totalExpenses) {
-	    return monthlyBudget - totalExpenses;
-	}*//*
-
-
-    }*/
 
     private Budget getMonthlyBudget() {
         File f = new File(file_monthly_budget);
@@ -163,9 +108,6 @@ public class Operations {
         return expenses;
     }
 
-    public void getBudgetaryLogs() {
-    }
-
     public void changePassword() {
         try {
             Scanner sc = new Scanner(System.in);
@@ -248,6 +190,43 @@ public class Operations {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void getBudgetaryLogs() {
+        System.out.println("1. DATE-WISE LOG");
+        System.out.println("2. MONTH-WISE LOG");
+        System.out.println("3. TOTAL BUDGET");
+        System.out.print("SELECT THE BUDGET LOG YOU WANT TO DISPLAY - ");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                getDateWiseLog();
+                break;
+            case 2:
+                getMonthWiseLog();
+                break;
+            case 3:
+                getTotalLog();
+                break;
+        }
+    }
+
+    private void getDateWiseLog() {
+        List<Expenses> e = getExpensesList();
+        List<Expenses> matchingElements = e.stream()
+                .filter(expenses -> "27-10-2022".equals(expenses.getExpense_date()))
+                .collect(Collectors.toList());
+        for (Expenses ex : matchingElements) {
+            System.out.println(ex.toString());
+        }
+    }
+
+    private void getMonthWiseLog() {
+
+    }
+
+    private void getTotalLog() {
     }
 }
 		
